24b0fc5525ea2cfa7374347149e01051c6a00192
Thomas Cheng <thomascheng81@gmail.com>
2017-09-17 23:34:46 -0400

    Start styling controls

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "font-awesome/css/font-awesome.min.css";
 import "./index.css";
 import registerServiceWorker from "./registerServiceWorker";
 import RPSEngine from "./RPSEngine";
@@ -7,42 +8,43 @@ const RESULT_COLORS = {
   lose: "red",
   draw: "lightgrey"
 };
-const log = ({ result }) => {
-  console.log("%c " + result, "color: " + RESULT_COLORS[result]);
-};
 
 const rockButton = document.getElementById("rock");
 const paperButton = document.getElementById("paper");
 const scissorsButton = document.getElementById("scissors");
+const aiMoveEl = document.getElementById("ai-move");
+
+const update = ({ result, playerMove, aiMove }) => {
+  aiMoveEl.dataset.move = aiMove;
+  aiMoveEl.dataset.result = result;
+  console.log("%c " + result, "color: " + RESULT_COLORS[result]);
+};
+
 
 const engine = new RPSEngine();
+
+const play = move => {
+  update(engine.play(move));
+};
 
 window.addEventListener("keydown", evt => {
   switch (evt.keyCode) {
     case 49:
-      log(engine.play("rock"));
+      play("rock");
       break;
     case 50:
-      log(engine.play("paper"));
+      play("paper");
       break;
     case 51:
-      log(engine.play("scissors"));
+      play("scissors");
       break;
     default:
       break;
   }
 });
 
-rockButton.addEventListener("click", () => {
-  log(engine.play("rock"));
-});
-
-paperButton.addEventListener("click", () => {
-  log(engine.play("paper"));
-});
-
-scissorsButton.addEventListener("click", () => {
-  log(engine.play("scissors"));
-});
+rockButton.addEventListener("click", () => { play("rock"); });
+paperButton.addEventListener("click", () => { play("paper"); });
+scissorsButton.addEventListener("click", () => { play("scissors"); });
 
 registerServiceWorker();
