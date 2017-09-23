408be1038a4ee10a8ee361116803bf95bf30dd5c
Thomas Cheng <thomascheng81@gmail.com>
2017-09-23 02:43:04 -0400

    Add empty state text

--- src/components/ResultsArea.js
+++ src/components/ResultsArea.js
@@ -84,7 +84,9 @@ class ResultsArea extends Component {
         </div>
       </StyledBoard>
     ) : (
-      <StyledBoard />
+      <StyledBoard>
+        Choose Rock, Paper or Scissors
+      </StyledBoard>
     );
   }
 }
